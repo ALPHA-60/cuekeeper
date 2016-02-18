3d0cb3b68209bc4c52427ef10ac5dd9edcb30425
Thomas Leonard <talex5@gmail.com>
2016-02-18 19:16:38 +0000

    More debug

--- .travis-dbg.sh
+++ .travis-dbg.sh
@@ -3,6 +3,7 @@ bash -ex .travis-opam.sh || echo Failed
 cd /home/travis/.opam/system/build/mirage-block-unix.2.1.0
 echo OUT
 cat *.out
+cat /home/travis/.opam/system/build/mirage-block-unix.2.1.0/_build/oUnit-*.log
 echo ERR
 cat *.err
 exit 1
