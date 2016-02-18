5069cd7eaae6a77e18063345f1e4169f473d47be
Thomas Leonard <talex5@gmail.com>
2016-02-18 19:39:06 +0000

    Use mirage-block-unix with debugging

--- .travis-dbg.sh
+++ .travis-dbg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ux
 bash -ex .travis-opam.sh || echo Failed
-cd /home/travis/.opam/system/build/mirage-block-unix.2.1.0
+cd /home/travis/.opam/system/build/mirage-block-unix.*
 echo OUT
 cat *.out
-cat /home/travis/.opam/system/build/mirage-block-unix.2.1.0/_build/oUnit-*.log
+cat /home/travis/.opam/system/build/mirage-block-unix.*/_build/oUnit-*.log
 echo ERR
 cat *.err
 exit 1
